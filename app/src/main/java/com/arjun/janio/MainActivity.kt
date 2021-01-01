c44a3d5efa8443753928634846c4563cc54fa5fa
iamarjun <arjunmanoj1995@gmail.com>
2021-01-01 14:16:05 +0530

    add necessary dependencies and create basic layout

--- app/src/main/java/com/arjun/janio/MainActivity.kt
+++ app/src/main/java/com/arjun/janio/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.arjun.janio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.arjun.janio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+        crossinline bindingInflater: (LayoutInflater) -> T) =
+        lazy(LazyThreadSafetyMode.NONE) {
+            bindingInflater.invoke(layoutInflater)
+        }
